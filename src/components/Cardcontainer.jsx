cb71307a2bc482dbbc5b0bf75eb58df4c2bc51b7
NikhilM09 <mahadiknikhil28@gmail.com>
2025-02-17 19:47:33 +0530

    API integration

--- src/components/Cardcontainer.jsx
+++ src/components/Cardcontainer.jsx
@@ -1,17 +1,64 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard'
+import { useState, useEffect } from 'react'
+import {  API_URL } from '../constants/config'
 
 const Cardcontainer = () => {
+  const [restaurantList, setRestaurantList] = useState([])
+  const [searchText, setSearchText] = useState("");
+
+
+  const handleSearchText = (val) =>{
+    setSearchText(val)
+  }
+
+  
+
+ const handleRating = () => {
+  const filteredData = restaurantList.filter((restaurant)=>{
+    return (restaurant.rating>=4.5)
+  }
+  )
+  setRestaurantList(filteredData)
+  console.log("filteredData", restaurantList)
+ } 
+
+ useEffect(()=>{
+  const getRestaurantData = async() =>{
+    try{
+      const response = await fetch(API_URL);
+      const data = await response.json()
+      console.log("carousel data",data?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info )
+      console.log("restaurant list",data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]) 
+      setRestaurantList(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+    catch(error){
+      console.log("error", error)
+    }
+  }
+  getRestaurantData();
+ }, [])
+
+console.log("page rendered")
   return (
     <>
     {/* <div>This is Cardcontainer component</div> */}
+    <button className="bg-gray-300 p-2 rounded-md shadow-md hover:bg-gray-400" onClick={()=>handleRating()}>Top rated restaurants</button>
+    <input className="border-2 border-black" type="text" value={searchText} onChange={(e)=>handleSearchText(e.target.value)}/>
+    <h1>{searchText}</h1>
     <div className="flex gap-4 px-4 py-4 flex-wrap justify-center">
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        {
+        restaurantList.map((restaurant)=>{
+            return <RestaurantCard
+            // rating={restaurant["rating"]}
+            // deliveryTime={restaurant.deliveryTime}
+            // name={restaurant.name}
+            //   cuisines={restaurant.cuisines}
+            //   location={restaurant.location}
+            {...restaurant?.info}
+            />
+          })
+        }
     </div>
     </>
   )
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import { IMG_URL } from '../constants/config'
 
-const RestaurantCard = () => {
+const RestaurantCard = ({name, avgRating, sla, cuisines, locality, cloudinaryImageId
+}) => {
+  // console.log("props", props)
+  // const {name, rating, deliveryTime, cuisines, location} = props
   return (
-    <div>
-        <div className="w-full md:max-w-[250px] max-w-[300px] h-[150px]">
-            <img className="w-full h-full object-cover rounded-[15px] " src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"/>
+    <div className="w-full md:max-w-[250px] max-w-[300px]">
+        <div className="w-full h-[150px]">
+            <img className="w-full h-full object-cover rounded-[15px] " src={IMG_URL+cloudinaryImageId}/>
         </div>
         <div className="px-2">
-            <h2 className="font-semibold text-2xl">Chinese Wok</h2>
+            <h2 className="font-semibold text-2xl">{name}</h2>
             <div className="flex gap-2">
-            <h2 className="text-[16px] font-semibold">⭐4.4</h2>
-            <h2 className="text-[16px] font-semibold">35-40 min</h2>
+            <h2 className="text-[16px] font-semibold">⭐{avgRating}</h2>
+            <h2 className="text-[16px] font-semibold">{sla?.deliveryTime} min</h2>
             </div>
         </div>
-        <p className="text-gray-500 px-3">Chinese, Asian, Tibetan, Desserts</p>
-        <p className="text-gray-500 px-3">Santacruz East</p>
+        <p className="text-gray-500 px-3">{cuisines.join(", ")}</p>
+        <p className="text-gray-500 px-3">{locality}</p>
     </div>
   )
 }
